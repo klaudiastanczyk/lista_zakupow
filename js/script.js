423bf7e85ce89ecaa8dd3c9a2f5565bdacdbc2c4
Klaudia Stanczyk <klaudia.stanczyk92@gmail.com>
2022-06-27 23:54:16 +0200

    add first css

--- js/script.js
+++ js/script.js
@@ -9,11 +9,13 @@ function categoriesAddListener(){
 function linkClickHandler(event){
   event.preventDefault();
   const clickedElement = event.target;
+  console.log('ddd', clickedElement)
   const href = clickedElement.getAttribute('href');
   const category = href.replace('#', '');
-  const dataCategory = document.querySelector(`[data-category=${category}]`);
-  dataCategory.classList.add('active');
-}
+  const categoryName = document.querySelector(`[data-category=${category}]`);
+  categoryName.classList.add('active');
+  console.log('categoryName', categoryName);
 
+}
 
 categoriesAddListener();
